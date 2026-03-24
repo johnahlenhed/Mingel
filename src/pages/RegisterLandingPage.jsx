ac7805f3ac2f5a1717f25bb6fa312c2b74952407
John Ahlenhed <johnahlenhed@gmail.com>
2026-03-24 12:55:34 +0100

    Register landing content, colors and initial layout

--- src/pages/RegisterLandingPage.jsx
+++ src/pages/RegisterLandingPage.jsx
@@ -11,12 +11,12 @@ function RegisterLandingPage() {
 
             <h1>Networking with <b>purpose</b></h1>
 
-            <p>
+            <p className={styles.bottomHeaderText}>
                 An evening where Sweden's best design students meet the employers of tomorrow
             </p>
 
-            <section>
-                <h5>THE CONCEPT</h5>
+            <section className={styles.mainSection}>
+                <h6>THE CONCEPT</h6>
                 <h3>Two parts.</h3>
                 <h3>One connection.</h3>
 
@@ -38,58 +38,55 @@ function RegisterLandingPage() {
                     <p>CONNECT</p>
                     <p>SAVE</p>
                 </div>
+                
+            </section>
 
-                <div className={styles.stats}>
-                    <div className={styles.employed}>
-                        <h5>94%</h5>
-                        <p>Employed</p>
-                    </div>
-                    <div className={styles.program}>
-                        <h5>2</h5>
-                        <p>Program</p>
-                    </div>
-                    <div className={styles.ranking}>
-                        <h5>#1</h5>
-                        <p>In Sweden</p>
-                    </div>
+            <section className={styles.stats}>
+                <div className={styles.employed}>
+                    <h5>94%</h5>
+                    <p>Employed</p>
                 </div>
-
-                <div className={styles.infoWrapper}>
-
-                    <h4>What you get</h4>
-
-                    <div className={styles.info}>
-                        <div className={styles.numberBox}>
-                            <h5>1</h5>
-                        </div>
-                        <p>Get inspired by tomorrow's designers - today</p>
-                    </div>
-                    <div className={styles.info}>
-                        <div className={styles.numberBox}>
-                            <h5>2</h5>
-                        </div>
-                        <p>Network with the companies shaping the future of design</p>
-                    </div>
-                    <div className={styles.info}>
-                        <div className={styles.numberBox}>
-                            <h5>3</h5>
-                        </div>
-                        <p>Find your next hire among job-ready design students</p>
-                    </div>
-
-                    <div className={styles.instructions}>
-                        <p>How it works</p>
-                        <p>Each participant receives a digital puzzle piece with a QR code. </p>
-                        <p>No business cards – scan each others puzzle pieces and expand your professional network.</p>
-                    </div>
-
-                    <p>Are you a student? Register here</p>
+                <div className={styles.program}>
+                    <h5>2</h5>
+                    <p>Program</p>
+                </div>
+                <div className={styles.ranking}>
+                    <h5>#1</h5>
+                    <p>In Sweden</p>
                 </div>
             </section>
 
-            <Link to="/register" className={styles.registerButton}>
-                Get Started
-            </Link>
+            <section className={styles.infoWrapper}>
+
+                <h4>What you get</h4>
+
+                <div className={styles.info}>
+                    <div className={`${styles.numberBox} ${styles.numberBoxAccent}`}>
+                        <h5>1</h5>
+                    </div>
+                    <p>Get inspired by tomorrow's designers - today</p>
+                </div>
+                <div className={styles.info}>
+                    <div className={styles.numberBox}>
+                        <h5>2</h5>
+                    </div>
+                    <p>Network with the companies shaping the future of design</p>
+                </div>
+                <div className={styles.info}>
+                    <div className={styles.numberBox}>
+                        <h5>3</h5>
+                    </div>
+                    <p>Find your next hire among job-ready design students</p>
+                </div>
+
+                <div className={styles.instructions}>
+                    <p>How it works</p>
+                    <p>Each participant receives a digital puzzle piece with a QR code. </p>
+                    <p>No business cards – scan each others puzzle pieces and expand your professional network.</p>
+                </div>
+
+                <p className={styles.ending}>Are you a student? Register here</p>
+            </section>
         </main>
     )
 }
