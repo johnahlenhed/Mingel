89a7da2b5bce42b417a1dcde8355ff240e22a0db
John Ahlenhed <johnahlenhed@gmail.com>
2026-03-24 11:45:02 +0100

    Register landing font and color

--- src/pages/RegisterLandingPage.jsx
+++ src/pages/RegisterLandingPage.jsx
@@ -4,18 +4,18 @@ import RegisterButton from '../components/RegisterButton.jsx'
 
 function RegisterLandingPage() {
     return (
-        <main className={styles.landingPage}>
-            <p>
+        <main className={styles.registerLandingPage}>
+            <p className={styles.topText}>
                 YRGO - DIGITAL DESIGNER - APRIL 22
             </p>
 
-            <h1>Networking with purpose</h1>
+            <h1>Networking with <b>purpose</b></h1>
 
             <p>
                 An evening where Sweden's best design students meet the employers of tomorrow
             </p>
 
-            <main>
+            <section>
                 <h5>THE CONCEPT</h5>
                 <h3>Two parts.</h3>
                 <h3>One connection.</h3>
@@ -85,7 +85,7 @@ function RegisterLandingPage() {
 
                     <p>Are you a student? Register here</p>
                 </div>
-            </main>
+            </section>
 
             <Link to="/register" className={styles.registerButton}>
                 Get Started
